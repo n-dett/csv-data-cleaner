8dfee461fe2e21c694a3bc180dc321a85fa7ab0d
n-dett <124851780+n-dett@users.noreply.github.com>
2025-04-08 19:46:44 -0500

    Parse column names

---
+++ csv-data-cleaner.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import argparse
+
+def main():
+    # Set up parser
+    parser = argparse.ArgumentParser();
+    parser.add_argument("file_path", Help="Path to the file")
+    args = parser.parse_args()
+    file_path = args.file_path
+
+    # Load csv into data frame
+    data_frame = pd.read_csv(file_path)
+
+
+    # Parse column names
+    column_names = data_frame.columns.tolist()
+    for column_name in column_names:
+        # Remove leading and trailing spaces
+        column_name.strip()
+        # Make all names lowercase
+        column_name.lower()
+        # Replace spaces with underscores
+        column_name.replace(" ", "_")
+        # Replace special characters with underscores
+        column_name.replace(r'[^\w]', '_', regex=True)
+
+
+
+    # Export cleaned data frame to csv
+    data_frame.to_csv("cleaned-data.csv")
+
+if __name__ == "__main__":
+    main()
